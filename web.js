99e115f1d7d7736af63ad84000538bca9988ca9a
Paige Ring <ubuntu@ip-172-31-44-14.us-west-2.compute.internal>
2013-07-11 19:01:14 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+var buffer = fs.readFileSync("index.html");
+
+var helloText = buffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(helloText);
 });
 
 var port = process.env.PORT || 5000;
